7922587e92877123c03c08bbb525a7ba8d469712
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-03-02 15:50:34 +0100

    mediaQ update

--- Ice cream yeehaw.js
+++ Ice cream yeehaw.js
@@ -1,6 +1,10 @@
 // radio buttons //
+$(document).ready(function(){
+
+
 document.querySelector('input[name="scoopbtn"]:checked') 
 
 document.querySelector('input[name="bowl-conebtn"]:checked') 
 
 
+}
