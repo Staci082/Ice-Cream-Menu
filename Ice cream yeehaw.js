0ee8ec1dc68f6a2d06f50f6cedf44cac1d529a29
Staci082 <sjjohnson082@gmail.com>
2023-03-03 10:38:26 +0100

    icons

--- Ice cream yeehaw.js
+++ Ice cream yeehaw.js
@@ -6,5 +6,9 @@ document.querySelector('input[name="scoopbtn"]:checked')
 
 document.querySelector('input[name="bowl-conebtn"]:checked') 
 
-
-}
+$(document).ready(function(){
+    $(".resetbtn").click(function(){
+        location.reload(true);
+    });
+});
+})
